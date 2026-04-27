af14c5bb716322e7e832520da7cf043a7143f5b6
Maxim.Kunz <maximkunz065@gmail.com>
2026-04-27 13:10:19 +0200

    M2

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import WorkoutList from './components/WorkoutList.vue'
+</script>
+
+<template>
+  <main>
+    <h1>Workout Tracker</h1>
+    <WorkoutList />
+  </main>
+</template>
---
+++ src/components/WorkoutList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const workouts = ref([
+  { id: 1, name: 'Bankdrücken', sets: 3 },
+  { id: 2, name: 'Kniebeugen', sets: 4 },
+  { id: 3, name: 'Kreuzheben', sets: 3 }
+])
+</script>
+
+<template>
+  <div class="workout-list">
+    <h2>Meine Workouts</h2>
+    <ul>
+      <li v-for="workout in workouts" :key="workout.id">
+        {{ workout.name }} ({{ workout.sets }} Sätze)
+      </li>
+    </ul>
+  </div>
+</template>
